5dd80bfc6974a4f7a8aa4091484e93204697d6b9
Mouhcine Fallahi <mohcinefallahi23@gmail.com>
2025-07-14 19:44:52 +0100

    fix: add path to puppeteer

--- controllers/notes.js
+++ controllers/notes.js
@@ -140,6 +140,7 @@ const downloadPdf = async (req, res) => {
     const browser = await puppeteer.launch({
       headless: true,
       args: ["--no-sandbox", "--disable-setuid-sandbox"],
+      executablePath: "/opt/render/.cache/puppeteer/chrome/linux-138.0.7204.94/chrome-linux64/chrome",
     });
     const page = await browser.newPage();
     await page.setContent(html, { waitUntil: "networkidle0" });
