38833f2cfba20c5b354df7f74746d43f9ea32ae4
Mouhcine Fallahi <mohcinefallahi23@gmail.com>
2025-07-22 09:19:35 +0100

    fix: add test script

--- test.js
+++ test.js
@@ -1,13 +1,6 @@
 const fs = require("fs");
 const path = require("path");
 
-const chromePath = path.join(
-  __dirname,
-  ".cache/puppeteer",
-  "chrome",
-  "linux-138.0.7204.94",
-  "chrome-linux64",
-  "chrome",
-);
+const chromePath = "/opt/render/.cache/puppeteer/chrome/linux-138.0.7204.94/chrome-linux64/chrome";
 
 console.log("Does Chromium exist?", fs.existsSync(chromePath), chromePath);
